3e6adf8217c8fc1d54ac505c4078821738385d6c
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-15 10:28:53 -0700

    Names completed

--- names/names.py
+++ names/names.py
@@ -10,17 +10,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# Return the list of duplicates in this data structure
+duplicates = list(set(names_1).intersection(set(names_2)))
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Grabbing a name from names_1 list
+# for name_1 in names_1:
+#     # Checking for a match in names_2
+#     for name_2 in names_2:
+#         # If there is a match add to duplicates.
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
