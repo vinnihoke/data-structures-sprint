5bc2224921bd439c7ef51e5f08b8f289847e9e89
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-15 11:43:43 -0700

    Added BigO for each names algo.

--- names/names.py
+++ names/names.py
@@ -14,6 +14,8 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+duplicates = []
+
 # * Return the list of duplicates in this data structure
 # Replace the nested for loops below with your improvements
 # Grabbing a name from names_1 list
@@ -25,7 +27,7 @@ f.close()
 #             duplicates.append(name_1)
 
 # ? Using set and intersect.
-duplicates = list(set(names_1).intersection(set(names_2)))
+# duplicates = list(set(names_1).intersection(set(names_2)))
 # https://www.w3schools.com/python/ref_set_intersection.asp
 # Creates a list with a set from names_1. Intersection is the difference between the two lists, therefore all the duplicates. We pass in another set from names_2.
 # Complexity is (n-1)*O(l) where l is max(len(s1),..,len(sn))
@@ -33,12 +35,11 @@ duplicates = list(set(names_1).intersection(set(names_2)))
 # runtime: 0.004992008209228516 seconds
 
 # ? Using list comprehension.
-# duplicates = [name for name in names_1 if name in names_2]
+duplicates = [name for name in names_1 if name in names_2]
 # This implementation should be O(n^2). It still needs to loop over each element and run a looped if check in names because of the "in" keyword.
 # runtime: 1.5957581996917725 seconds
 
 # ? Using SLL.
-# duplicates = []
 # sll = SLL()
 
 # for name in names_1:
@@ -47,7 +48,7 @@ duplicates = list(set(names_1).intersection(set(names_2)))
 # for name in names_2:
 #     if sll.search(name):
 #         duplicates.append(name)
-# Worked but DAMN lol!!!
+# Worked but DAMN lol!!! O(c^n)
 # runtime: 32.25076985359192 seconds
 
 end_time = time.time()
